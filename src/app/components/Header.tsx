d2489b7ee5b799c2d8e51ca0bbcf65ce96243a94
Tabish Raza <ts764raza@gmail.com>
2024-09-30 10:18:49 -0700

    Initial commit for Next.js project

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+  <>
+  <div className='min-h-screen m-w-screen'>
+        <nav className='flex justify-center items-center mt-2w bg-gray-700 text-white  h-10 list-none gap-6 text-xl'>
+            <li>Home</li>
+            <li>About me</li>
+            <li>Contact</li>
+        </nav>
+        <section>
+            <h1 className='text-7xl text-center mt-24'>Hi, Its Tabish Raza</h1>
+            <h2 className='text-center text-7xl mt-11'>I am a student of GIAIC</h2>
+        </section>
+        </div>
+        </>
+    
+  )
+}
+
+export default Header

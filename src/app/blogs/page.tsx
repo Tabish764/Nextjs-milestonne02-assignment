e4fa5c3ffa0c5e26ebc3727fb4207d63f334e27e
Tabish Raza <ts764raza@gmail.com>
2024-11-09 01:55:39 +0500

    'first'

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/Hero'
+
 import Blogs from '../components/Blogs'
 
 const page = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from './components/Header'
+
 import Hero from './components/Hero'
-import Footer from './components/Footer'
+
 const page = () => {
   return (
     <div className=' w-full  0'>
